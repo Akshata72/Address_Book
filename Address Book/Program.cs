3fbe69caf3ec06eaf4c2cc5b9a940723007c2da5
Akshata72 <sableakshata880@gmail.com>
2022-03-23 19:15:22 +0530

    Added:UC3-Edit_A_Contact

--- Address Book/Program.cs
+++ Address Book/Program.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
 
 namespace AddressBook
 {
-    class ContactDetails
+    public class ContactDetails
     {
         public String First_Name;
         public String Last_Name;
@@ -12,8 +17,6 @@ namespace AddressBook
         public String ZIP;
         public String Phone_Number;
         public String Email_Address;
-
-
         public ContactDetails(string First_Name, string Last_Name, string Address, string City, string State, string ZIP, string Phone_Number, string Email_Address)
         {
             this.First_Name = First_Name;
@@ -28,45 +31,23 @@ namespace AddressBook
         public string tostring()
         {
             return " Details of  " + First_Name + " " + Last_Name + " are: " + "Address: " + Address + " city: " + City + "\n"
-                                             +"                            " + "State: " + State + " Zip: " + ZIP + "\n"
-                                             +"                            " + "Phone Number: " + Phone_Number + "\n"
-                                             +"                            " + "Email: " + Email_Address;
+                                             + "                            " + "State: " + State + " Zip: " + ZIP + "\n"
+                                             + "                            " + "Phone Number: " + Phone_Number + "\n"
+                                             + "                            " + "Email: " + Email_Address;
 
         }
-
     }
-
     class program
     {
-        private  ArrayList contactDetailslist;
-        private Dictionary<string, ContactDetails> contactDetailsMap;
+        private ArrayList contactDetailslist;
+        private Dictionary<string, ContactDetails> contactDetailsMap = new Dictionary<string, ContactDetails>();
         public program()
         {
             contactDetailslist = new ArrayList();
             contactDetailsMap = new Dictionary<string, ContactDetails>();
         }
-
-        public void AddDetails(string First_Name, string Last_Name, string Address, string City, string State, string ZIP, string Phone_Number, string Email_Address)
+        public void AddDetails()
         {
-            ContactDetails contactDetails = new ContactDetails(First_Name, Last_Name, Address, City, State, ZIP, Phone_Number, Email_Address);
-            contactDetailslist.Add(contactDetails);
-            contactDetailsMap.Add(First_Name,contactDetails);
-        }
-        public void ComputeDetails()
-        {
-            foreach (ContactDetails contact in contactDetailslist)
-            {
-                Console.WriteLine(contact.tostring());
-            }
-
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Address Book System");
-            program details = new program();
-
-
             Console.WriteLine("Enter a First Name:");
             string First_Name = Console.ReadLine();
             Console.WriteLine("Enter a Last Name:");
@@ -78,15 +59,92 @@ namespace AddressBook
             Console.WriteLine("Enter a State:");
             string State = Console.ReadLine();
             Console.WriteLine("Enter a ZIP:");
-            string ZIP =(Console.ReadLine());
+            string ZIP = (Console.ReadLine());
             Console.WriteLine("Enter a Phone Number:");
-            string Phone_Number =(Console.ReadLine());
+            string Phone_Number = (Console.ReadLine());
             Console.WriteLine("Enter a Email_Address:");
             string Email_Address = Console.ReadLine();
-            details.AddDetails(First_Name, Last_Name, Address, City, State, ZIP, Phone_Number, Email_Address);
-            details.ComputeDetails();
+            ContactDetails contactDetails = new ContactDetails(First_Name, Last_Name, Address, City, State, ZIP, Phone_Number, Email_Address);
+            contactDetailslist.Add(contactDetails);
+            contactDetailsMap.Add(First_Name, contactDetails);
         }
-
+        public void EditDetails(string key)
+        {
+            if (contactDetailsMap.ContainsKey(key))
+            {
+                Console.WriteLine("Enter a New First Name:");
+                string First_Name = Console.ReadLine();
+                Console.WriteLine("Enter a New Last Name:");
+                string Last_Name = Console.ReadLine();
+                Console.WriteLine("Enter a New Address:");
+                string Address = Console.ReadLine();
+                Console.WriteLine("Enter a New City:");
+                string City = Console.ReadLine();
+                Console.WriteLine("Enter a New State:");
+                string State = Console.ReadLine();
+                Console.WriteLine("Enter a New ZIP:");
+                string ZIP = (Console.ReadLine());
+                Console.WriteLine("Enter a New Phone Number:");
+                string Phone_Number = (Console.ReadLine());
+                Console.WriteLine("Enter a New Email_Address:");
+                string Email_Address = Console.ReadLine();
+                ContactDetails contactDetails = new ContactDetails(First_Name, Last_Name, Address, City, State, ZIP, Phone_Number, Email_Address);
+                contactDetailslist.Add(contactDetails);
+                contactDetailsMap[key] = contactDetails;
+            }
+            else
+            {
+                Console.WriteLine("Key Not Found.");
+            }
+        }
+        public void ComputeDetails()
+        {
+            foreach (ContactDetails contact in contactDetailslist)
+            {
+                Console.WriteLine(contact.tostring());
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int Option = 0;
+            Console.WriteLine("Welcome To Address Book");
+            program details = new program();
+            do
+            {
+                Console.WriteLine("1: To Add a Contact Details.");
+                Console.WriteLine("2: To Compute Details.");
+                Console.WriteLine("3: To Edit a Contact Details.");
+                Console.WriteLine("0: To Exit");
+                try
+                {
+                    Option = int.Parse(Console.ReadLine());
+                    switch (Option)
+                    {
+                        case 1:
+                            details.AddDetails();
+                            continue;
+                        case 2:
+                            details.ComputeDetails();
+                            continue;
+                        case 3:
+                            Console.WriteLine("Enter a First Name to Edit");
+                            string key = Console.ReadLine();
+                            details.EditDetails(key);
+                            break;
+                        case 0:
+                            Console.WriteLine("Exit");
+                            break;
+                        default:
+                            Console.WriteLine("Wrong key");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please choose option");
+                }
+            }
+            while (Option != 0);
+        }
     }
 }
-    
